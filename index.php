15bfa01b4bc747844eafe247f5195d6e22b83e73
DJ <magicm3rk@gmail.com>
2017-04-21 15:41:43 -0400

    Added php files from original project start, on the road again!

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+  
+        <form action="tempdata.php" method="get">
+        DataName: <input type="text" name="unitId"><br>
+        Temp: <input type="text" name="currentTemp"><br>
+        Humidity: <input type="text" name="humidity"><br>
+        <input type="submit">
+        </form>
+
+              
+    </body>
+</html>
---
+++ tempdata.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+
+
+<?php
+$string = file_get_contents("./tempdata.JSON") or die("Unable to open file!");
+$json_a = json_decode($string, true);
+
+
+$json_a[$_GET['unitId']]['time0'] = date("h:i:sa");
+array_pop($json_a[$_GET['unitId']]['temp0']);
+array_unshift($json_a[$_GET['unitId']]['temp0'], $_GET['currentTemp']);
+if($_GET['humidity'])
+{
+    $json_a[$_GET['unitId']]['humidity'] = $_GET['humidity'];
+}
+echo $json_a;
+
+file_put_contents("./tempdata.JSON", json_encode($json_a));
+echo "success!";
+echo "The time is " . date("h:i:sa");
+
+?>
+<h2>the end</h2>
+</body>
+</html>
