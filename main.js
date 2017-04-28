dd65bc139ada05a106d64338896a9187bdf45b6c
DJ <magicm3rk@gmail.com>
2017-04-28 16:48:09 -0400

    added chart.js refer chart RESTORE POINT if this doesn't pan out'

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function GetClock(){
         document.getElementById('live-clock').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
 }
 
-
+/*
 function drawCanvas (referNum){
         var ctx = document.getElementById("refer-canvas" + referNum).getContext("2d");
         var tempUnsafe = c.getContext("2d");
@@ -35,12 +35,59 @@ function drawCanvas (referNum){
         ctx.lineTo(144, 72);
         ctx.stroke();
 }
+*/
+
+
+
+
 
 $(document).ready(function() {
     GetClock();
     setInterval(GetClock,1000);
-    drawCanvas(1);
+    //drawCanvas(1);
+
+
+    //create chart 1
+    var chart = new CanvasJS.Chart("referCanvas1",
+        {
+            animationEnabled: true,
+            backgroundColor: "skyblue",
+            title:{
+            text: "Refer 1",
+            fontSize: 30
+            },
+            axisX: {
+                valueFormatString: "MMM",
+                interval:1,
+                intervalType: "month"
+            },
+            axisY:{
+                includeZero: false
+
+            },
+            data: [
+            {
+                type: "spline",
+
+                dataPoints: [
+                { x: 0, y: 39.15 },
+                { x: 1, y: 38.12},
+                { x: 2, y: 39.61},
+                { x: 3, y: 40.16 },
+                { x: 4, y: 56.12 },
+                { x: 5, y: 59.12 },
+                { x: 6, y: 41.01 },
+                { x: 7, y: 42.02 },
+                { x: 8, y: 41.10 },
+                { x: 9, y: 39.08 },
+                { x: 10, y: 39.10 },
+                { x: 11, y: 38.10 }
+                ]
+            }
+            ]
+    });
     
+        chart.render();
 
 });
 
