22c75e173ed84250457a6c1e1a5cc4c8e8a63a4b
DJ <magicm3rk@gmail.com>
2017-04-30 20:12:58 -0400

    worked on data handling for chart (canvasJS) and added a text file for tracking needed config options

--- main.js
+++ main.js
@@ -19,8 +19,10 @@ function GetClock(){
         document.getElementById('live-clock').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
 }
 
-function redraw(chart) {
-    chart.render();
+function updateChart(chart1, referData) {
+    
+    chart1.options.data[0].dataPoints = referData;
+    chart1.render();
 }
 /*
 function drawCanvas (referNum){
@@ -48,7 +50,39 @@ $(document).ready(function() {
     GetClock();
     setInterval(GetClock,1000);
     //drawCanvas(1);
-
+    referData = 
+[[
+                { x: 1, y: 45.12},
+                { x: 2, y: 39.61},
+                { x: 3, y: 40.16 },
+                { x: 4, y: 56.12 },
+                { x: 5, y: 59.12 },
+                { x: 6, y: 41.01 },
+                { x: 7, y: 42.02 },
+                { x: 8, y: 41.10 },
+                { x: 9, y: 39.08 },
+                { x: 10, y: 39.10 },
+                { x: 11, y: 38.10 },
+                { x: 12, y: 31.15 },
+                { x: 13, y: 34.12},
+                { x: 14, y: 39.61},
+                { x: 15, y: 40.16 },
+                { x: 16, y: 56.12 },
+                { x: 17, y: 59.12 },
+                { x: 18, y: 41.01 },
+                { x: 19, y: 42.02 },
+                { x: 20, y: 63.10 },
+                { x: 21, y: 39.08 },
+                { x: 22, y: 39.10 },
+                { x: 23, y: 38.10 },
+                { x: 24, y: 31.15 },
+                { x: 25, y: 38.12},
+                { x: 26, y: 33.61},
+                { x: 27, y: 40.16 },
+                { x: 28, y: 56.12 },
+                { x: 29, y: 59.12 },
+                { x: 30, y: 41.01 }
+], []];
 
     //create chart 1
     var maxTemp = 41;
@@ -59,8 +93,8 @@ $(document).ready(function() {
             axisX: {
                 valueFormatString: "#",
                 interval:3,
-               
-                labelFontSize: 10,
+                
+                labelFontSize: 10
                 
             },
             axisY:{
@@ -74,10 +108,11 @@ $(document).ready(function() {
                                 
                                 value:41,
                                 color:"red",
-                                label : "41" + String.fromCharCode(176),
-                                thickness:.5,
+                                label : maxTemp + String.fromCharCode(176),
+                                thickness:.7,
                                 labelBackgroundColor:"skyblue",
-                                labelColor: "red"
+                                labelColor: "red",
+                                labelFontSize: 16
                             }
                             ]
 
@@ -87,25 +122,15 @@ $(document).ready(function() {
                 type: "spline",
 
                 dataPoints: [
-                { x: 1, y: 38.12},
-                { x: 2, y: 39.61},
-                { x: 3, y: 40.16 },
-                { x: 4, y: 56.12 },
-                { x: 5, y: 59.12 },
-                { x: 6, y: 41.01 },
-                { x: 7, y: 42.02 },
-                { x: 8, y: 41.10 },
-                { x: 9, y: 39.08 },
-                { x: 10, y: 39.10 },
-                { x: 11, y: 38.10 },
-                { x: 12, y: 39.15 }
-                
+                { x: 1, y: 41.12},
                 ]
             }
             ]
     });
+    chart.render();
 
-        chart.render();
+    setInterval(updateChart, 60000, chart, referData[0]);
+    
 
 });
 
