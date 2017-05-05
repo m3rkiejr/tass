3c3b78d91aa50d3709f555685f47bc1f1a3f98d4
DJ <magicm3rk@gmail.com>
2017-05-05 19:09:47 -0400

    changed WU image into an iframe from dynamic loading

--- main.js
+++ main.js
@@ -466,7 +466,7 @@ window.onload = function() {
 
     setInterval(updateTChart, 6000, chart, referData, numOfCharts, sensorName, timeData); //to alter charts, change data in referData arrays
     setTimeout(updateFB, 10000); //updates FB iframe every 10 minutes, will fix with better code later
-    setInterval(updateWU, 60000);
+    setInterval(updateWU, 600000);
   
  
 };
