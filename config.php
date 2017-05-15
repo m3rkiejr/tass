d17eb0f42ebafb00a7bca0146dd8abd54c90c16c
DJ <magicm3rk@gmail.com>
2017-05-15 01:01:48 -0400

    added code for config changes, config php, and created php page to alter data stored in tempdata.json

---
+++ config.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+       
+        <div style="font-size:23px"> 
+            <form action="tempdata2.php" method="get">
+                <h2>Sensor Calibration</h2>
+            SensorNumber: <input type="text" name="unitId"><br>
+            Calibration Offset (negative numbers to increase, positive to decrease)<br>
+            For ex.  current 34 Degrees, desired, 32 offset would be positive 2 degrees</br>
+            Enter offset here: <input type="text" name="calibrationOffset"><br>
+            
+                <h2>Sensor Name Change</h2>
+                <h3>Name change for Refer only on sensor 1-6, and Outdoor, Front, Middle, and Kitchen only for 6-10</h3>
+
+            SensorNumber: <input type="text" name="unitIdNameChange"><br>
+            Enter New Name here(limit 12 Characters): <input type="text" name="unitNewName"><br>
+            <input type="submit">
+            </form>
+        </div>
+
+              
+    </body>
+</html>
---
+++ tempdata2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My second PHP page</h1>
+
+
+
+<?php
+$string = file_get_contents("./tempdata.JSON") or die("Unable to open file!");
+$json_a = json_decode($string, true);
+
+if($_GET['unitId'])
+{
+    $json_a['calibration'][($_GET['unitId'])-1] =  $_GET['calibrationOffset'];
+    echo "Calibration successful for" . $json_a['sensorName'][($_GET['unitId'])-1];
+}
+if ($_GET['unitNewName']) {
+    $json_a['sensorName'][($_GET['unitIdNameChange']-1)] = substr($_GET['unitNewName'], 0, 12); //sets new sensor name after truncating to length of 12
+    echo "New name changed to " . substr($_GET['unitNewName']) . "for sensorName: " .  $json_a['sensorName'][($_GET['unitIdNameChange']-1)];
+}
+
+
+file_put_contents("./tempdata.JSON", json_encode($json_a));
+echo "success!";
+echo "The time is " . date("H:i:s");
+
+?>
+<h2>the end</h2>
+</body>
+</html>
